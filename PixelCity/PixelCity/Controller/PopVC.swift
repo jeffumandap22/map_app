9ea55c6a95498410b440d6766698361764365b73
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-07 20:26:01 +0800

    Build PopVC, Presenting PopVC when UICollectionViewCell is tapped

---
+++ PixelCity/PixelCity/Controller/PopVC.swift
@@ -0,0 +1,38 @@
+//
+//  PopVC.swift
+//  PixelCity
+//
+//  Created by Jeffrey Umandap on 07/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    var passedImage: UIImage!
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        popImageView.image = passedImage
+        addDoubleTap()
+    }
+
+    func addDoubleTap() {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+}
